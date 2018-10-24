b81842c31a0767fff00a9ed6da79d354f9ff8261
Gehaha <GeHaha0@163.com>
2018-10-24 13:03:09 +0800

    innerclass

--- Thinking_In_Java/class/innerclasses/DotThis.java
+++ Thinking_In_Java/class/innerclasses/DotThis.java
@@ -14,8 +14,10 @@ public class DotThis{
 	 return new Inner();
 	}
 	public static void main(String[] args){
-		DotThis dt = new DotThis();
+		DotThis dt = new DotThis(); 
 		DotThis.Inner dti = dt.inner();
+		// 要想直接创建内部类的对象，必须使用外部类的对象来创建该
+		//内部类的对象
 		dti.outer().f();
 		}
 	
